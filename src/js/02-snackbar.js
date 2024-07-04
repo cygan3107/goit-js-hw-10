7c695a0044fc01ede7e9242530c6f86a378c84e6
cygan3107 <krzysztof.malon@onet.pl>
2024-07-04 13:06:08 +0200

    snackbard-continue

--- src/js/02-snackbar.js
+++ src/js/02-snackbar.js
@@ -1,1 +1,39 @@
+// Opisany w dokumentacji
+import iziToast from 'izitoast';
+// Kolejny import stylów
+import 'izitoast/dist/css/iziToast.min.css';
 
+const formRef = document.querySelector('.form');
+const formsItemsRef = {
+  delay: document.querySelector('input[name="delay"]'),
+  state: document.querySelector('input[name="state"]'),
+};
+
+formRef.addEventListener('submit', event => {
+  event.preventDefault();
+  for (let i = 0; i < formsItemsRef.amount.value; i += 1) {
+    createPromise(
+      i + 1,
+      Number(formsItemsRef.delay.value) + i * Number(formsItemsRef.state.value)
+    )
+      .then(value => {
+        Notify.success(value);
+      })
+      .catch(err => {
+        Notify.failure(err);
+      });
+  }
+});
+
+function createPromise(delay) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res(`✅ Fulfilled promise in ${delay}ms`);
+      } else {
+        rej(`❌ Rejected promise in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
