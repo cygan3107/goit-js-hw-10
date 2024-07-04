a8bdc40cc1febbdf5b97a9b0042744003e9214b7
cygan3107 <krzysztof.malon@onet.pl>
2024-07-04 12:57:48 +0200

    timer-done

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -1,16 +1,16 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-const startBtnRef = document.querySelector('button[data-start]');
-const inputDateRef = document.querySelector('#datetime-picker');
-const timerValueRef = {
+const startBtn = document.querySelector('button[data-start]');
+const inputDate = document.querySelector('#datetime-picker');
+const timerValue = {
   days: document.querySelector('span[data-days]'),
   hours: document.querySelector('span[data-hours]'),
   minutes: document.querySelector('span[data-minutes]'),
   seconds: document.querySelector('span[data-seconds]'),
 };
 
-startBtnRef.disabled = true;
+startBtn.disabled = true;
 let dateNow = Date.now();
 let timeLeft = 0;
 let selectedDate = 0;
@@ -23,17 +23,17 @@ const options = {
   onClose(selectedDates) {
     if (Date.parse(selectedDates[0]) < dateNow) {
       Notify.failure('Please choose a date in the future');
-      startBtnRef.disabled = true;
+      startBtn.disabled = true;
     } else {
       selectedDate = Date.parse(selectedDates[0]);
-      startBtnRef.disabled = false;
+      startBtn.disabled = false;
     }
   },
 };
 
-const fp = flatpickr(inputDateRef, options);
+const fp = flatpickr(inputDate, options);
 
-startBtnRef.addEventListener('click', onStartBtnClick);
+startBtn.addEventListener('click', onStartBtnClick);
 
 function onStartBtnClick() {
   const interval = setInterval(() => {
@@ -41,10 +41,10 @@ function onStartBtnClick() {
     timeLeft = selectedDate - dateNow;
     if (timeLeft > 0) {
       const convertTimeLeft = convertMs(timeLeft);
-      timerValueRef.days.textContent = convertTimeLeft.days;
-      timerValueRef.hours.textContent = convertTimeLeft.hours;
-      timerValueRef.minutes.textContent = convertTimeLeft.minutes;
-      timerValueRef.seconds.textContent = convertTimeLeft.seconds;
+      timerValue.days.textContent = convertTimeLeft.days;
+      timerValue.hours.textContent = convertTimeLeft.hours;
+      timerValue.minutes.textContent = convertTimeLeft.minutes;
+      timerValue.seconds.textContent = convertTimeLeft.seconds;
     } else {
       clearInterval(interval);
     }
