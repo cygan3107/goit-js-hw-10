2c7cb6b554f07eeb460965adaf10fd5dc26e4cc8
cygan3107 <krzysztof.malon@onet.pl>
2024-07-11 12:25:55 +0200

    02

--- src/js/02-snackbar.js
+++ src/js/02-snackbar.js
@@ -3,33 +3,31 @@ import iziToast from 'izitoast';
 // Kolejny import stylów
 import 'izitoast/dist/css/iziToast.min.css';
 
-const formRef = document.querySelector('.form');
-const formsItemsRef = {
+const form = document.querySelector('.form');
+const formsItems = {
   delay: document.querySelector('input[name="delay"]'),
   state: document.querySelector('input[name="state"]'),
 };
 
-formRef.addEventListener('submit', event => {
+form.addEventListener('submit', event => {
   event.preventDefault();
-  for (let i = 0; i < formsItemsRef.delay.value; i += 1) {
-    createPromise(i, Number(formsItemsRef.delay.value))
+  
+    createPromise(delay,state)
       .then(delay => {
         iziToast.success({message:`✅ Fulfilled promise in ${delay}ms`});
       })
       .catch(delay => {
         iziToast.warning({message:`❌ Rejected promise in ${delay}ms`});
       });
-  }
-});
+    });
 
-function createPromise(delay) {
-return new Promise((res, rej) => {
+function createPromise(delay,state) {
+return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
-      if (shouldResolve) {
-        res(`✅ Fulfilled promise in ${delay}ms`);
+      if (state === `fulfilled`) {
+        resolve(delay);
       } else {
-        rej(`❌ Rejected promise in ${delay}ms`);
+        reject(delay);
       }
     }, delay);
   });
